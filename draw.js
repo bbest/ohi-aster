10870482791b71f2e4e0ade32a9bb55b349e3b08
Parker Abercrombie <parker@TheOkoriGroup.com>
2014-09-03 11:33:41 -0700

    Now draws outer and inner rings.

--- draw.js
+++ draw.js
@@ -1,5 +1,3 @@
-//var widths = [0.5, 0.5, 1.0];
-//var lengths = [20, 40, 80];
 
 var dataset = [
    { width: 20, length: 50, id: 1},
@@ -19,12 +17,14 @@ var color = d3.scale.category20();
 
 var pie = d3.layout.pie()
     .value(function(d) { return d.width; });
-//var pie = d3.layout.pie()
-//    .sort(null);
 
 var arc = d3.svg.arc()
         .innerRadius(innerRadius)
         .outerRadius(function (d) { return (radius - innerRadius) * (d.data.length / 100.0) + innerRadius; });
+
+var outlineArc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(radius);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -32,12 +32,19 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var path = svg.selectAll("path")
+var path = svg.selectAll(".solidArc")
     .data(pie(dataset))
   .enter().append("path")
     .attr("fill", function(d, i) { return color(i); })
-    .attr("d", arc);
+    .attr("class", "solidArc")
+    .attr("stroke", "gray")
+    .attr("d", arc)
+    .on("click", function (d) { console.log(d); });
 
-//    .attr("d", arc);
-
-//    .attr("d", function(d, i) { return arc(lengths[i]); });
+var outerPath = svg.selectAll(".outlineArc")
+    .data(pie(dataset))
+  .enter().append("path")
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("class", "outlineArc")
+    .attr("d", outlineArc);
