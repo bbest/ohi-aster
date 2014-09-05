6cca00cf64d05c35c7f7ebc2d93e8573e53a9a83
bbest <bbest@nceas.ucsb.edu>
2014-09-05 16:13:48 -0700

    working now with OHI data

--- draw.js
+++ draw.js
@@ -1,33 +1,24 @@
-
-var dataset = [
-   { width: 20, length: 50, id: 1},
-   { width: 20, length: 60, id: 2},
-   { width: 20, length: 50, id: 3},
-   { width: 20, length: 70, id: 3},
-   { width: 20, length: 80, id: 3},
-];
-
 var width = 460,
     height = 300,
-    radius = Math.min(width, height) / 2;
-
-var innerRadius = 0.3 * radius;
-
-var color = d3.scale.category20();
+    radius = Math.min(width, height) / 2,
+    innerRadius = 0.3 * radius;
 
 var pie = d3.layout.pie()
+    .sort(null)
     .value(function(d) { return d.width; });
 
 var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([0, 0])
   .html(function(d) {
-    return "<strong>ID:</strong> <span style='color:red'>" + d.data.id + "</span>";
+    return d.data.label + ": <span style='color:orangered'>" + d.data.score + "</span>";
   });
 
 var arc = d3.svg.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(function (d) { return (radius - innerRadius) * (d.data.length / 100.0) + innerRadius; });
+  .innerRadius(innerRadius)
+  .outerRadius(function (d) { 
+    return (radius - innerRadius) * (d.data.score / 100.0) + innerRadius; 
+  });
 
 var outlineArc = d3.svg.arc()
         .innerRadius(innerRadius)
@@ -41,20 +32,54 @@ var svg = d3.select("body").append("svg")
 
 svg.call(tip);
 
-var path = svg.selectAll(".solidArc")
-    .data(pie(dataset))
-  .enter().append("path")
-    .attr("fill", function(d, i) { return color(i); })
-    .attr("class", "solidArc")
-    .attr("stroke", "gray")
-    .attr("d", arc)
-    .on('mouseover', tip.show)
-    .on('mouseout', tip.hide);
+d3.csv('data/aster_data.csv', function(error, data) {
 
-var outerPath = svg.selectAll(".outlineArc")
-    .data(pie(dataset))
-  .enter().append("path")
-    .attr("fill", "none")
-    .attr("stroke", "gray")
-    .attr("class", "outlineArc")
-    .attr("d", outlineArc);
+  data.forEach(function(d) {
+    d.id     =  d.id;
+    d.order  = +d.order;
+    d.color  =  d.color;
+    d.weight = +d.weight;
+    d.score  = +d.score;
+    d.width  = +d.weight;
+    d.label  =  d.label;
+  });
+  // for (var i = 0; i < data.score; i++) { console.log(data[i].id) }
+  
+  var path = svg.selectAll(".solidArc")
+      .data(pie(data))
+    .enter().append("path")
+      .attr("fill", function(d) { return d.data.color; })
+      .attr("class", "solidArc")
+      .attr("stroke", "gray")
+      .attr("d", arc)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
+
+  var outerPath = svg.selectAll(".outlineArc")
+      .data(pie(data))
+    .enter().append("path")
+      .attr("fill", "none")
+      .attr("stroke", "gray")
+      .attr("class", "outlineArc")
+      .attr("d", outlineArc);  
+
+
+  // calculate the weighted mean score
+  var score = 
+    data.reduce(function(a, b) {
+      //console.log('a:' + a + ', b.score: ' + b.score + ', b.weight: ' + b.weight);
+      return a + (b.score * b.weight); 
+    }, 0) / 
+    data.reduce(function(a, b) { 
+      return a + b.weight; 
+    }, 0);
+
+  svg.append("svg:text")
+    .attr("class", "aster-score")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle") // text-align: right
+    .text(Math.round(score));
+
+});
+
+
--- prep_data.R
+++ prep_data.R
@@ -6,24 +6,32 @@ suppressPackageStartupMessages({
   require(dplyr)
   require(knitr)
   require(RColorBrewer)
+  require(stringr)
   suppressWarnings(require(ohicore)) # devtools::install_github('ohi-science/ohicore')
 })
 
+# variables
+yr = 2014
+url_scores = sprintf('https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez%d/scores.csv', yr)
+url_labels = sprintf('https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez%d/layers/rgn_labels.csv', yr)
+url_goals  = sprintf('https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez%d/conf/goals.csv', yr)
+csv_d = 'data/scores.csv'
+csv_g = 'data/goals.csv'
+csv_p = 'data/aster_data.csv'
+flower_png = 'flower.png'
 refresh_data = F
 
+dir.create('data', showWarnings=F)
+
 # get data
-csv_d = 'data/scores.csv'
 if (!file.exists(csv_d) | refresh_data){
-  dir.create('data', showWarnings=F)
   
   # get scores
-  url_scores = 'https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez2014/scores.csv'
   tmp_scores = tempfile(fileext='.csv')
   download.file(url_scores, tmp_scores, method='curl')
   scores = read.csv(tmp_scores)
   
   # get labels
-  url_labels = 'https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez2014/layers/rgn_labels.csv'
   tmp_labels = tempfile(fileext='.csv')
   download.file(url_labels, tmp_labels, method='curl')
   labels = read.csv(tmp_labels)
@@ -54,9 +62,7 @@ d = read.csv(csv_d) %>%
   arrange(desc(score))
 
 # get goals
-csv_g = 'data/goals.csv'
 if (!file.exists(csv_g) | refresh_data){
-  url_goals = 'https://raw.githubusercontent.com/OHI-Science/ohi-global/master/eez2014/conf/goals.csv'
   tmp_goals = tempfile(fileext='.csv')
   download.file(url_goals, tmp_goals, method='curl')
   g = read.csv(tmp_goals)
@@ -82,7 +88,7 @@ x = g %>%
   arrange(order_color)
 
 # plot
-png('flower.png', width=800, height=800, res=150)
+png(flower_png, width=800, height=800, res=150)
 PlotFlower(
   main       = '',
   lengths    = x$score,
@@ -93,6 +99,17 @@ PlotFlower(
   disk       = 0.4, 
   max.length = 100, cex=2, label.cex=0.5, label.offset=0.13)
 dev.off()
-system('open flower.png')
+system(sprintf('open %s', flower_png))
 
-# aggregate data into sin
+# aggregate data into single csv for d3 rendering
+p = data.frame(
+  id     = x$goal,
+  order  = x$order_color,
+  score  = round(x$score),
+  weight = x$weight,
+  color  = colorRampPalette(brewer.pal(10, 'Spectral'), space='Lab')(nrow(x)),
+  label  = str_trim(gsub('\\\\n',' ', x$name_flower))
+)
+write.csv(p, csv_p, row.names=F)
+cat(paste('Extracted weighted average score:', round(d %>% filter(region_label=='GLOBAL' & goal=='Index') %>% select(center=score)), '\n'))
+cat(paste('Calculated weighted average score:', round(weighted.mean(x$score, x$weight)), '\n'))
